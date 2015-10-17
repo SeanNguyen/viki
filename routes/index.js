affa715f37383b20926f7585af5f4681dc54eb39
Sean Nguyen <sean.nguyentrong@gmail.com>
2015-10-17 22:17:02 +0800

    added Gruntfile.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var cheerio = require('cheerio');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/', function(req, res) {
+  url = 'http://www.viki.com';
+
+    request(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html);
+            console.log($('.c-item a').attr('href'));
+			res.send(html);
+        }
+    })
 });
 
 module.exports = router;
