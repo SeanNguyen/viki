6cb96ca27baf5715162e1a0dd87a581112e97e2b
Sean Nguyen <sean.nguyentrong@gmail.com>
2015-10-18 18:06:38 +0800

    added phantom js

--- routes/index.js
+++ routes/index.js
@@ -2,51 +2,86 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
+var phantom = require('phantom');
+
 
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  url = 'http://www.viki.com';
+	url = 'http://www.viki.com';
 
-    request(url, function(error, response, html){
-        if(!error){
-            var $ = cheerio.load(html);
+	//phantomjs
+	phantom.create(function (ph) {
+		ph.createPage(function (page) {
+			page.onLoadFinished = function() {
+				console.log('Page loaded finished');
+				res.send(page.content);
+		    	ph.exit();
+		    };
 
-            //get all items that need to analyze
-            var items = [];
+			page.open(url, function (status) {
+				console.log('Status: ' + status);
+				if(status === 'success') {
+					page.evaluate(function(){});
+				} else {
+					request(url, function(error, response, html){
+						if(!error){
+							var repeatCount = countDuplicatedItems(html);
+							res.send(repeatCount);
+						}
+					});
+					ph.exit();
+				}
+		    });
+		});
+	}, 
+	{
+		dnodeOpts: {
+			weak: false
+		}
+	});
 
-            $('.item').each(function() {
-            	var url = extractUrlFromItem(this, $);            	
-				items.push(url);
-            });
-            $('.c-item').each(function() {
-				var url = extractUrlFromItem(this, $);            	
-				items.push(url);
-            });
-
-            var repeatCount = { videos: {}, channels: {}, celebrities: {} };
-
-            for (var i = items.length - 1; i >= 0; i--) {
-            	var url = items[i];
-            	if(!url) {
-	            	continue;
-	            }
-            	if(url.indexOf('tv/') > -1) {
-            		var id = url.substring(url.indexOf('tv/'));
-            		repeatCount.channels[id] = (repeatCount.channels[id] != null) ? repeatCount.channels[id] + 1 : 1;
-            	} else if(url.indexOf('videos/') > -1) {
-            		var id = url.substring(url.indexOf('videos/'));
-            		repeatCount.videos[id] = (repeatCount.videos[id] != null) ? repeatCount.videos[id] + 1 : 1;
-            	} else if(url.indexOf('celebrities/') > -1) {
-					var id = url.substring(url.indexOf('celebrities/'));
-					repeatCount.celebrities[id] = (repeatCount.celebrities[id] != null) ? repeatCount.celebrities[id] + 1 : 1;
-            	}
-            };
-
-			res.send(repeatCount);
-        }
-    })
 });
+
+function countDuplicatedItems(htmlContent) {
+	var $ = cheerio.load(htmlContent);
+
+	//get all items that need to analyze
+	var items = [];
+
+	$('.item').each(function() {
+		var url = extractUrlFromItem(this, $);
+		items.push(url);
+	});
+	$('.menu-dropdown').each(function() {
+		var url = extractUrlFromItem(this, $);
+		items.push(url);
+	});
+	$('.c-item').each(function() {
+		var url = extractUrlFromItem(this, $);
+		items.push(url);
+	});
+
+	var repeatCount = { videos: {}, channels: {}, celebrities: {} };
+
+	for (var i = items.length - 1; i >= 0; i--) {
+		var url = items[i];
+		if(!url) {
+			continue;
+		}
+		if(url.indexOf('tv/') > -1) {
+			var id = url.substring(url.indexOf('tv/'));
+			repeatCount.channels[id] = (repeatCount.channels[id] != null) ? repeatCount.channels[id] + 1 : 1;
+		} else if(url.indexOf('videos/') > -1) {
+			var id = url.substring(url.indexOf('videos/'));
+			repeatCount.videos[id] = (repeatCount.videos[id] != null) ? repeatCount.videos[id] + 1 : 1;
+		} else if(url.indexOf('celebrities/') > -1) {
+			var id = url.substring(url.indexOf('celebrities/'));
+			repeatCount.celebrities[id] = (repeatCount.celebrities[id] != null) ? repeatCount.celebrities[id] + 1 : 1;
+		}
+	}
+	return repeatCount;
+}
 
 function extractUrlFromItem(htmlItem, $) {
 	// console.log(htmlItem);
